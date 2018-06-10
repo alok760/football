59cf1a6fd6ea7e7df2918b7bc1f0b40ad25af10f
Alok Kumar <alok760@gmail.com>
2018-06-10 20:40:22 +0530

    function to create default values in the database

--- post.php
+++ post.php
@@ -1,14 +1,22 @@
 <?php
+$conn = new mysqli("localhost", "root", "", "football");
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+  echo 
+for( $i = 0; $i<5; $i++ ){
+  $sql = "INSERT INTO f1 (adidas)
+  VALUES (0)";
+  if ($conn->query($sql) === TRUE) {
+
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+}
+  echo "table created succesfully";
+
   $mytime=gettimeofday();
-  echo "$mytime[sec]";
-
-  $filename = "newfile.txt";
-   $file = fopen( $filename, "w" );
-
-   if( $file == false ) {
-      echo ( "Error in opening new file" );
-      exit();
-   }
-   fwrite( $file, "$mytime[sec]" );
-   fclose( $file );
+  $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+  fwrite( $myfile, "$mytime[sec]" );
+  fclose($myfile);
  ?>
