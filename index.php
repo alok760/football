2a7da4c68a0f5617114f175fa512ff3587af87d6
Alok Kumar <alok760@gmail.com>
2018-06-11 01:46:57 +0530

    reducing file size by using require

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
             <form action="ind.php" method="POST">
             <div class="row wrap_row">
                 <div class="col-sm-6">
-                    <div class="col-sm-11">
+                    <div class="col-sm-12">
                         <div class="row my_row border">
-                                    <button class="btn btn-block col-sm-3" name="one" value="adidas">Adidas</buttom>
+                                    <button class="btn btn-block col-sm-3 " name="one" value="adidas">Adidas</buttom>
                                     <button class="btn btn-block col-sm-3" name="one" value="goal">goal</buttom>
-                                    <button class="btn btn-block col-sm-3" name="one" value="mcdonadls">Mcdonalds</button>
+                                    <button class="btn btn-block col-sm-3" name="one" value="mcdonalds">Mcdonalds</button>
                                     <div class="mx-auto text-center ss"><b>A</b></div>
                         </div>
 
@@ -32,14 +32,14 @@
                         <div class="row my_row border">
                                     <button class="btn btn-block col-sm-3" name="one" value="wandagroup">Wanda-Group</buttom>
                                     <button class="btn btn-block col-sm-3" name="one" value="budweiser">Budweiser</buttom>
-                                    <button class="btn btn-block col-sm-3" name="one" value="penalty">Attri</button>
+                                    <button class="btn btn-block col-sm-3" name="one" value="penalty">penalty</button>
                                     <div class="mx-auto text-center ss"><b>C</b></div>
                         </div>
 
                         <div class="row my_row border">
-                                    <button class="btn btn-block col-sm-3">Attri</buttom>
-                                    <button class="btn btn-block col-sm-3">Attri</buttom>
-                                    <button class="btn btn-block col-sm-3">Attri</button>
+                                    <button class="btn btn-block col-sm-3" name="one" value="qatarairways">Qatar Airways</buttom>
+                                    <button class="btn btn-block col-sm-3" name="one" value="mengniu1">Mengniu</buttom>
+                                    <button class="btn btn-block col-sm-3" name="one" value="gazprom">Gazprom</button>
                                     <div class="mx-auto text-center ss"><b>D</b></div>
                         </div>
 
@@ -49,32 +49,32 @@
                 </div>
 
                 <div class="col-sm-6">
-                    <div class="col-sm-11">
+                    <div class="col-sm-12">
                         <div class="row my_row border">
-                                    <button class="btn btn-block col-sm-3">Attri</buttom>
-                                    <button class="btn btn-block col-sm-3">Attri</buttom>
-                                    <button class="btn btn-block col-sm-3">Attri</button>
+                                    <button class="btn btn-block col-sm-3" name="one" value="cocacola">Coca-cola</buttom>
+                                    <button class="btn btn-block col-sm-3" name="one" value="wandagroup1">Wanda-Group</buttom>
+                                    <button class="btn btn-block col-sm-3" name="one" value="substitution">Substitution</button>
                                     <div class="mx-auto text-center ss"><b>E</b></div>
                         </div>
 
                         <div class="row my_row border">
-                                    <button class="btn btn-block col-sm-3">Attri</buttom>
-                                    <button class="btn btn-block col-sm-3">Attri</buttom>
-                                    <button class="btn btn-block col-sm-3">Attri</button>
+                                    <button class="btn btn-block col-sm-3" name="one" value="visa">Visa</buttom>
+                                    <button class="btn btn-block col-sm-3" name="one" value="misses">Misses</buttom>
+                                    <button class="btn btn-block col-sm-3" name="one" value="fifa">FIFA</button>
                                     <div class="mx-auto text-center ss"><b>F</b></div>
                         </div>
 
                         <div class="row my_row border">
-                                    <button class="btn btn-block col-sm-3">Attri</buttom>
-                                    <button class="btn btn-block col-sm-3">Attri</buttom>
-                                    <button class="btn btn-block col-sm-3">Attri</button>
+                                    <button class="btn btn-block col-sm-3" name="one" value="interestingmoment">Interesting-M</buttom>
+                                    <button class="btn btn-block col-sm-3" name="one" value="closeupshots">Close-Up Shots</buttom>
+                                    <button class="btn btn-block col-sm-3" name="one" value="hyundaimotor">Hyundai-motor</button>
                                     <div class="mx-auto text-center ss"><b>G</b></div>
                         </div>
 
                         <div class="row my_row border">
-                                    <button class="btn btn-block col-sm-3">Attri</buttom>
-                                    <button class="btn btn-block col-sm-3">Attri</buttom>
-                                    <button class="btn btn-block col-sm-3">Attri</button>
+                                    <button class="btn btn-block col-sm-3" name="one" value="cardred">Card-Red</buttom>
+                                    <button class="btn btn-block col-sm-3" name="one" value="cardyellow">Card-Yellow</buttom>
+                                    <button class="btn btn-block col-sm-3" name="one" value="save">Save</button>
                                     <div class="mx-auto text-center ss"><b>H</b></div>
                         </div>
 
